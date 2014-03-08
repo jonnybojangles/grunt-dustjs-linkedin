b9c4052ce56f5c3e75cbc458daaff8a70283eb0c
coryroloff <cory@brothersroloff.com>
2014-03-08 15:07:38 -0600

    remove temporary testing files

--- default_options.js
+++
@@ -1,1 +0,0 @@
-(function(){dust.register("../default_options",body_0);function body_0(chk,ctx){return chk.write("<!DOCTYPE HTML><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><title></title></head><body></body></html>");}return body_0;})();
