a274508371cb41685b938e784975d7ae192dde25
coryroloff <cory@brothersroloff.com>
2014-03-19 11:04:21 -0500

    refactoring

--- tasks/dust.js
+++ tasks/dust.js
@@ -8,156 +8,151 @@ function Builder(grunt) {
   this.grunt = grunt;
   dust.helpers = helpers;
   this.dustOptimizers = _.clone(dust.optimizers);
-  dust.optimizers.format = function(ctx, node) { return node; };
-  var amd = dust.compile(this.load('amd-wrapper.dust'), 'amd-wrapper');
-  var commonjs = dust.compile(this.load('commonjs-wrapper.dust'), 'commonjs-wrapper');
-  var defaultHelper = dust.compile(this.load('dust-helper.dust'), 'dust-helper');
-  var qHelper = dust.compile(this.load('q-helper.dust'), 'q-helper');
-  dust.loadSource(amd);
-  dust.loadSource(commonjs);
-  dust.loadSource(defaultHelper);
-  dust.loadSource(qHelper);
+  this.load();
 }
 
-Builder.prototype.build = function(task) {
-  var self = this;
-  var files = task.files;
-  var options = task.options({
-    name: self.name,
-    optimizers: {},
-    wrapper: false,
-    helper: false,
-    dependencies: {}
-  });
+Builder.prototype = {
+  load: function() {
+    var self = this;
+    dust.optimizers.format = function(ctx, node) { return node; };
+    _.each([
+      'amd-wrapper',
+      'commonjs-wrapper',
+      'dust-helper',
+      'q-helper'
+    ], function(name) {
+      var file = path.join(__dirname, name + '.dust');
+      var source = self.grunt.file.read(file);
+      dust.loadSource(dust.compile(source, name));
+    });
+  },
 
-  _.each(files, function(file) {
-    self.grunt.file.write(file.dest, self.compile(file, options));
-  });
-};
+  build: function(task) {
+    var self = this;
+    var files = task.files;
+    var options = task.options({
+      name: self.name,
+      optimizers: {},
+      wrapper: false,
+      helper: false,
+      dependencies: {}
+    });
+    _.each(files, function(file) {
+      self.grunt.file.write(file.dest, self.compile(file, options));
+    });
+  },
 
-Builder.prototype.compile = function(file, options) {
-  var grunt = this.grunt;
-  var src = file.src;
-  var dest = file.dest;
-  var source = grunt.file.read(src);
-  var name = this.result(options.name, this, {file: file}, options);
-  this.reset();
-  dust.optimizers = _.extend(dust.optimizers, options.optimizers);
-  var data = {
-    file: file,
-    name: name,
-    compiled: dust.compile(source, name)
-  };
+  compile: function(file, options) {
+    var grunt = this.grunt;
+    var src = file.src;
+    var dest = file.dest;
+    var source = grunt.file.read(src);
+    var name = this.result(options.name, this, {file: file}, options);
+    this.reset();
+    dust.optimizers = _.extend(dust.optimizers, options.optimizers);
+    var data = {
+      file: file,
+      name: name,
+      compiled: dust.compile(source, name)
+    };
 
-  return this.wrap(data, options);
-};
+    return this.wrap(data, options);
+  },
 
-Builder.prototype.name = function(data, options) {
-  var file = data.file;
-  var out = path.join(
-    path.dirname(file.dest),
-    path.sep,
-    path.basename(file.dest, path.extname(file.dest))
-  );
+  wrapper: function(format, data, options) {
+    var self = this;
+    var wrapped;
+    var context = dust.makeBase({
+      compiled: data.compiled,
+      name: data.name,
+      helper: data.helper,
+      dependencies: _.map(options.dependencies, function(value, key) {
+        return {
+          key: key,
+          value: value
+        };
+      })
+    });
+    dust.render(format + '-wrapper', context, function(err, out) {
+      if(err) self.grunt.fail.warn(err);
+      wrapped = out;
+    });
+    return wrapped;
+  },
 
-  return out.replace(file.orig.dest + path.sep, '');
-};
+  name: function(data, options) {
+    var file = data.file;
+    var out = path.join(
+      path.dirname(file.dest),
+      path.sep,
+      path.basename(file.dest, path.extname(file.dest))
+    );
 
-Builder.prototype.wrap = function(data, options) {
-  var helper = this.result(options.helper, this, data, options) || '';
-  var helperOutput;
+    return out.replace(file.orig.dest + path.sep, '');
+  },
 
-  switch(helper) {
-    case 'dust':
-    case 'q':
-      helperOutput = this.helper(helper, data, options);
-      break;
-    default:
-      helperOutput = helper;
+  wrap: function(data, options) {
+    var helper = this.result(options.helper, this, data, options) || '';
+    var helperOutput;
+    switch(helper) {
+      case 'dust':
+      case 'q':
+        helperOutput = this.helper(helper, data, options);
+        break;
+      default:
+        helperOutput = helper;
+    }
+    data = _.extend(data, {
+      helper: helperOutput
+    });
+    var result = this.result(options.wrapper, this, data, options);
+    var wrapper = result || data.compiled;
+    var wrapperOutput;
+    switch(wrapper) {
+      case 'amd':
+      case 'commonjs':
+        wrapperOutput = this.wrapper(wrapper, data, options);
+        break;
+      default:
+        wrapperOutput = wrapper;
+        break;
+    }
+    return wrapperOutput;
+  },
+
+  helper: function(format, data, options) {
+    var self = this;
+    var wrapped;
+    var context = dust.makeBase({
+      name: data.name,
+      compiled: data.compiled,
+      dependencies: _.map(options.dependencies, function(value, key) {
+        return {
+          key: key,
+          value: value
+        };
+      })
+    });
+    dust.render(format + '-helper', context, function(err, out) {
+      if(err) self.grunt.fail.warn(err);
+      wrapped = out;
+    });
+    return wrapped;
+  },
+
+  result: function(value, context) {
+    if(_.isFunction(value)) {
+      var args = Array.prototype.slice.call(arguments, 2);
+      return value.apply(context, args);
+    }
+    return value;
+  },
+
+  reset: function() {
+    _.each(this.dustOptimizers, function(optimizer, name) {
+      dust.optimizers[name] = optimizer;
+    });
   }
-
-  data = _.extend(data, {
-    helper: helperOutput
-  });
-
-  var wrapper = this.result(options.wrapper, this, data, options) || data.compiled;
-  var wrapperOutput;
-
-  switch(wrapper) {
-    case 'amd':
-    case 'commonjs':
-      wrapperOutput = this.wrapper(wrapper, data, options);
-      break;
-    default:
-      wrapperOutput = wrapper;
-      break;
-  }
-
-  return wrapperOutput;
-};
-
-Builder.prototype.wrapper = function(format, data, options) {
-  var self = this;
-  var wrapped;
-  var context = dust.makeBase({
-    compiled: data.compiled,
-    name: data.name,
-    helper: data.helper,
-    dependencies: _.map(options.dependencies, function(value, key) {
-      return {
-        key: key,
-        value: value
-      };
-    })
-  });
-
-  dust.render(format + '-wrapper', context, function(err, out) {
-    if(err) self.grunt.fail.warn(err);
-    wrapped = out;
-  });
-
-  return wrapped;
-};
-
-Builder.prototype.helper = function(format, data, options) {
-  var self = this;
-  var wrapped;
-  var context = dust.makeBase({
-    name: data.name,
-    compiled: data.compiled,
-    dependencies: _.map(options.dependencies, function(value, key) {
-      return {
-        key: key,
-        value: value
-      };
-    })
-  });
-
-  dust.render(format + '-helper', context, function(err, out) {
-    if(err) self.grunt.fail.warn(err);
-    wrapped = out;
-  });
-
-  return wrapped;
-};
-
-Builder.prototype.result = function(value, context) {
-  if(_.isFunction(value)) {
-    var args = Array.prototype.slice.call(arguments, 2);
-    return value.apply(context, args);
-  }
-
-  return value;
-};
-
-Builder.prototype.reset = function() {
-  _.each(this.dustOptimizers, function(optimizer, name) {
-    dust.optimizers[name] = optimizer;
-  });
-};
-
-Builder.prototype.load = function(filename) {
-  return this.grunt.file.read(path.join(__dirname, filename));
 };
 
 module.exports = function(grunt) {
