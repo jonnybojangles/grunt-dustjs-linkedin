ed3f733a24c6c5e291506f6701658cc58dc1543f
coryroloff <cory@brothersroloff.com>
2014-05-03 13:31:27 -0500

    add partials support to the requirejs plugin

--- public/dust.js
+++ public/dust.js
@@ -1,4 +1,4 @@
-define(['module'], function(module) {
+define(['module', 'text'], function(module, text) {
   var masterConfig = (module.config && module.config()) || {};
 
   return {
@@ -8,16 +8,27 @@ define(['module'], function(module) {
         return;
       }
       var helperName = masterConfig.helper || false;
-      var deps = ['text', 'dust-full'];
+      var deps = ['dust-full'];
+      var url = masterConfig.url = masterConfig.url || config.baseUrl;
+      var ext = masterConfig.ext = masterConfig.ext || '.dust';
+      var file = name;
+      
+      if(file && file[0] !== '.') file = url + file;
+      if(file.indexOf(ext, file.length - ext.length) === -1) file += ext;
       if(typeof helperName === 'string') deps.push(helperName + '-helper');
-      req(deps, function(text, dust, helperFunc) {
-        var helper = helperFunc || helperName || function() {};
-        var url = masterConfig.url = masterConfig.url || config.baseUrl;
-        var ext = masterConfig.ext = masterConfig.ext || '.dust';
-        var file = name;
-        if(file && file[0] !== '.') file = url + file;
-        if(file.indexOf(ext, file.length - ext.length) === -1) file += ext;
-        text.get(req.toUrl(file), function(source) {
+
+      text.get(req.toUrl(file), function(source) {
+        var rx = /\{>"([^"]+)"[^/]\/\}/g;
+        var match;
+
+        while((match = rx.exec(source)) !== null) {
+          var dep = match[1];
+          if(deps.indexOf(dep) !== -1) continue;
+          deps.push(module.id + '!' + match[1]);
+        }
+
+        req(deps, function(dust, helperFunc) {
+          var helper = helperFunc || helperName || function() {};
           dust.loadSource(dust.compile(source, name));
           onload(helper(name));
         });
